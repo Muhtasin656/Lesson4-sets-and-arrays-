41fbd16245ac001541acf126c230c1714d5d399f
Muhtasin656 <muhtasinmasrur656@gmail.com>
2025-08-17 16:19:26 +1000

    new

---
+++ Activity.py
@@ -0,0 +1,18 @@
+set1={1,2,3}
+print(set1)
+
+# set with mixed data types
+set2={1,1.2,3,2.1,"set"}
+print(set2)
+
+# sets can not have duplicates
+set3={1,2,2,3,4,4,5,6,6,6,6,}
+print(set3)
+
+# set can also be made from list
+set4=set([1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9])
+print(set4)
+
+# removing first element from the set
+set1.pop()
+print(set1)
